4650b067299f6849414156449fefc86139dae1f8
Eodh <eodh@odinnson.com>
2022-12-04 20:58:50 +0100

    Add create, retrieve, update, partial_update and destroy functions

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -7,6 +7,7 @@ from profiles_api import views
 
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')
+router.register('profile', views.UserProfileViewSet) #No need basename because queryset define auto
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
